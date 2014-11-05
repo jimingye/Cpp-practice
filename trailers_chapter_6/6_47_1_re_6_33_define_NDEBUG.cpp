cbe97835a3deb725849e4e7a22096810ff5da2f5
lawlietye <jimingye@gmail.com>
2014-11-05 22:54:42 +0800

    11/05 practice

---
+++ trailers_chapter_6/6_47_1_re_6_33_define_NDEBUG.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#define NDEBUG                                               //关闭调试器。
+
+using namespace::std;
+
+void vec(vector<int> ivec)
+{
+	#ifdef NDEBUG
+	    cerr << _ _fun_ _<<": array size is" << ivec.size()<<endl;
+	#endif
+	static size_t i=0;                                       //i需定义为局部静态变量，不然每次调用均会执行i的使用与销毁，导致程序无限循环ivec[1]的值。
+	if(i != ivec.size())
+	{
+		cout << ivec[i]<<" ";
+		++i;
+		vec(ivec);                                           //函数中调用自身，形成递归。
+	}
+}
+
+int main()
+{
+	vector<int> ivec1;
+	int n;
+	cout << "please enter numbers:" << endl;
+	while(cin >> n)
+		ivec1.push_back(n);
+	vec(ivec1);
+	return 0;
+}
